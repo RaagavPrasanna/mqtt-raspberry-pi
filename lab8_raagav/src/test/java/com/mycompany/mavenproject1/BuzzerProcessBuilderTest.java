bc7c5c96a276b76b486f36ee4464aa97be9de690
raagav.prasanna <raagav.pras@gmail.com>
2022-11-14 15:08:43 -0500

    Added tests for the process builders

---
+++ lab8_raagav/src/test/java/com/mycompany/mavenproject1/BuzzerProcessBuilderTest.java
@@ -0,0 +1,35 @@
+package com.mycompany.mavenproject1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import com.mycompany.mavenproject1.BuzzerProcessBuilder;
+
+/**
+ *
+ * @author Carlton Davis and Raagav Prasanna
+ */
+
+/* Test class for ProcessBuilderEx class */
+public class BuzzerProcessBuilderTest {
+    
+    public BuzzerProcessBuilder processInstance;
+    
+    //Constructor for the test class
+    public BuzzerProcessBuilderTest() {
+        String pathAndFile = "src/main/Python/Doorbell.py";
+        processInstance = new BuzzerProcessBuilder(pathAndFile);
+    } 
+    
+    /**
+     * Test of startProcess method, of class ProcessBuilderEx.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testStartProcess() throws Exception {
+        System.out.println("startProcess");
+        
+        //Determine whether the process input stream is null
+        String expResult = processInstance.startProcess();
+        assertNotNull(expResult);
+    }
+}
